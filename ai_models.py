072089722b40a67ba992d2eb5c0553792a55f24a
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-01-20 17:13:32 +0530

    test this

--- ai_models.py
+++ ai_models.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
 
 def initiate_huggingface_model(api_key=None):
-    client = OpenAI(api_key=api_key, base_url="https://router.huggingface.co/v1")
-    return client
+    try: 
+        client = OpenAI(api_key=api_key, base_url="https://router.huggingface.co/v1")
+        return client
+    except Exception as e:
+        raise Exception("Error initiating Huggingface model: ", e)
